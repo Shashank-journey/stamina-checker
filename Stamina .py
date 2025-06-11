4f5978d0e02fa4c84baab68ff97ec04b8782b68b
Shashank Shekhar <shashankshekhar.shyam@gmail.com>
2025-06-11 09:49:12 +0530

    Add files via upload

---
+++ Stamina .py
@@ -0,0 +1,7 @@
+stamina= int(input("enter ur stamina lvl(0 to 100):"))
+if stamina  >= 80 :
+  print("Ready for the battle chosen one ")
+elif  stamina >= 40 :
+  print("u are tired but u can still fight")
+else:
+  print("fall back warrior u need to heal in order to save ashina ")
